d3dd735813c582c530a6b8bc84f9062d87372340
Kleyber Rojas <bonarja@gmail.com>
2019-06-18 15:24:17 -0400

    Updates

---
+++ precache-manifest.4218c969ef1160d94fd56d18c4129d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28ddc2453350d9dcf709f00c739e5bb",
+    "url": "/microsass/index.html"
+  },
+  {
+    "revision": "1832ccc045165e4c4414",
+    "url": "/microsass/static/css/main.f26eb971.chunk.css"
+  },
+  {
+    "revision": "ad2a21e955a13d794dc6",
+    "url": "/microsass/static/js/2.55ab6349.chunk.js"
+  },
+  {
+    "revision": "1832ccc045165e4c4414",
+    "url": "/microsass/static/js/main.958d9003.chunk.js"
+  },
+  {
+    "revision": "4a63e212e32cfbd624e4",
+    "url": "/microsass/static/js/runtime~main.9fbd2224.js"
+  }
+]);
